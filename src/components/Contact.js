2d15c0129c53db4d558f2817e19e4c275bb6b76a
abdul kalam <vishalbaig6@gmail.com>
2023-02-18 23:07:28 +0500

    version 2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,28 +1,26 @@
 import React from 'react'
-
+import "../components/contact.css"
 function Contact() {
   return (
-    <div className='h-screen' style={{backgroundImage: `url("./contact1.jpg")`,
-    backgroundRepeat:'no-repeat',
-    backgroundSize:'cover',
-  
-    }}>
-      <div>
-        <p className='text-xl font-mono text-center mt-10'>
-          we are available 24/7 in your servies
-        </p>
+    <div id='main' className='h-screen' style={{backgroundImage:`url("./coffee.jpg")`,
+    backgroundRepeat:"no-repeat",
+    backgroundSize:"cover"}}>
 
-      </div>
-
-      <div className='flex mt-10 '>
-        <div className='m-auto flex flex-col gap-3 p-3'>
-        <input className='border-2 w-52 p-2' type="text" placeholder='Name'/>
-        <input className='border-2 w-52 p-2' type="text" placeholder='email'/>
-        <input className='border-2 w-52 h-24' type="text" placeholder='message'/>
-        <button className='border-2 bg-amber-300 hover:bg-amber-500 duration-500 text-xl'>Submit</button>
+      <div className='form'>
+        <div>
+        <h1> contact us</h1>
         </div>
-      
+        
+        <div className='inputs'>
+          <input className='input1' type="text" placeholder='Name'/>
+          <input className='input1' type="text" placeholder='Email'/>
+          <input className='input1' type="text" placeholder='Message'/>
+          
+        </div>
+
+        <button className='button1'>submit</button>
       </div>
+
     </div>
   )
 }
